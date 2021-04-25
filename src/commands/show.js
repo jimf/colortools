323c2d4ef7ce9a2aa4e96a1f1417b93e10cb5260
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2021-04-25 09:57:52 -0400

    Implement show "long" format

--- src/commands/show.js
+++ src/commands/show.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk');
 const stringLength = require('string-length');
 const table = require('text-table');
+const truncate = require('cli-truncate');
 const Config = require('../config');
 const ui = require('../ui');
 const util = require('../util');
@@ -14,20 +15,36 @@ Show information on a color.
 Available options:
   --help, -h             This help
   --format=<format>      Specify a different output format. Available formats:
-                         json
+                         json, long
   --json                 Shorthand for '--format=json'
+  --long, -l             Shorthand for '--format=long'
   -1                     (The numeric digit one) Force single-column output.
                          Applies to the default format only.
+  --columns=<columns>    Comma-separated list of columns to show (long format).
+                         Column names: color, hex, rgb, hsl, matches, similar
+  --[no-]headers         Show or hide column headers (long format)
+  --[no-]truncate        Truncate lines that extend beyond the tty width
+                         (long format)
 `.trim();
 
 exports.parseOptions = {
     boolean: [
+        'headers',
         'json',
+        'truncate',
         '1',
     ],
     string: [
+        'columns',
         'format',
     ],
+    default: {
+        headers: true,
+        truncate: true,
+    },
+    alias: {
+        long: 'l',
+    },
 };
 
 /**
@@ -35,15 +52,69 @@ exports.parseOptions = {
  *
  * @param {object[]} data Array of color results
  * @param {object} options Configuration options
- * @param {string} options.format Output format. One of: "json".
+ * @param {string} [options.format] Output format. One of: "json", "long".
  *   Anything other value will result in the default format.
- * @param {boolean} options.columns Specific to the default format. Outputs the results into columns.
+ * @param {boolean} [options.columns] Specific to the default format. Outputs the results into columns.
+ * @param {boolean} [options.headers] Show column headers
  * @param {number} [options.maxWidth] Maxiumum output width in tty columns
+ * @param {object} show Data components to display
+ * @param {boolean} [options.truncate] Truncate lines to maxWidth
  * @return {string} Run result
  */
 function template(data, options) {
     if (options.format === 'json') {
         return JSON.stringify(data.length === 1 ? data[0] : data);
+    }
+
+    if (options.format === 'long') {
+        const tableData = data.map(result => {
+            ['color', 'hex', 'rgb', 'hsl', 'match', 'mostSimilar']
+            const cols = [];
+
+            if (options.show.color) {
+                cols.push(chalk.bgHex(result.hex)('     '));
+            }
+
+            ['hex', 'rgb', 'hsl'].forEach(datum => {
+                if (options.show[datum]) {
+                    cols.push(result[datum]);
+                }
+            });
+
+            if (options.show.matches) {
+                cols.push(
+                    result.matches.length === 0
+                        ? 'NO MATCHES'
+                        : result.matches.map(m => m.name).join(',')
+                );
+            }
+
+            if (options.show.mostSimilar) {
+                cols.push(
+                    result.mostSimilar.length === 0
+                        ? 'NO SIMILAR'
+                        : result.mostSimilar.map(m => `${m.match.toFixed(2)}%|${m.name}`).join(',')
+                );
+            }
+
+            return cols;
+        });
+
+        if (options.headers) {
+            tableData.unshift([
+                ['color', 'Color'],
+                ['hex', 'HEX'],
+                ['rgb', 'RGB'],
+                ['hsl', 'HSL'],
+                ['matches', 'Matches'],
+                ['mostSimilar', 'Similar'],
+            ].filter(([datum]) => options.show[datum]).map(([, header]) => header));
+        }
+
+        const output = table(tableData, { stringLength });
+        return options.maxWidth && options.truncate
+            ? output.split('\n').map(line => truncate(line, options.maxWidth)).join('\n')
+            : output;
     }
 
     const swatches = data.map(result => {
@@ -99,8 +170,28 @@ exports.run = async state => {
         return state;
     }
 
+    if (state.opts.columns) {
+        const showColumns = state.opts.columns.split(',');
+        const valid = ['color', 'hex', 'rgb', 'hsl', 'matches', 'similar'];
+        const invalid = showColumns.filter(col => !valid.includes(col));
+
+        if (invalid.length > 0) {
+            state.options.writeStderr(`Invalid column${invalid.length === 1 ? '' : 's'} specified with '--columns': ${invalid.join(', ')}`);
+            state.options.writeStderr(`Use: ${valid.join(', ')}`);
+            state.error = true;
+            state.done = true;
+            return state;
+        }
+
+        state.opts.columns = showColumns;
+    }
+
     if (state.opts.json) {
         state.opts.format = 'json';
+    }
+
+    if (state.opts.long) {
+        state.opts.format = 'long';
     }
 
     const config = Config(state.options);
@@ -182,7 +273,17 @@ exports.run = async state => {
     state.options.writeStdout(template(data, {
         columns: !!state.options.stdoutColumns && !state.opts[1],
         format: state.opts.format,
+        headers: state.opts.headers,
         maxWidth: state.options.stdoutColumns,
+        show: {
+            color: state.opts.columns ? state.opts.columns.includes('color') : true,
+            hex: state.opts.columns ? state.opts.columns.includes('hex') : true,
+            rgb: state.opts.columns ? state.opts.columns.includes('rgb') : true,
+            hsl: state.opts.columns ? state.opts.columns.includes('hsl') : true,
+            matches: state.opts.columns ? state.opts.columns.includes('matches') : true,
+            mostSimilar: state.opts.columns ? state.opts.columns.includes('similar') : true,
+        },
+        truncate: state.opts.truncate,
     }));
 
     return state;
